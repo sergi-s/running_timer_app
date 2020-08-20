46bee26c5cd05bdeb02a2fbfc21e0f8112f858c9
sergi-s <52626795+sergi-s@users.noreply.github.com>
2020-08-20 17:24:44 +0200

    Add a training session

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,57 +3,146 @@ import Card from "./Card";
 
 function App() {
   setInterval(getTime, 1000);
-  const [BUTT, setBUTT] = useState(true);
-  const [initSate, setSate] = useState(
-    new Date().toLocaleTimeString().replace("AM", "").replace("PM", "")
-  );
+  //training
+  const [initTime, setTime] = useState(new Date());
+  const [initStartT, setStartT] = useState();
+  const [initTraining, setTraining] = useState();
+  const [isTraining, setIsTraining] = useState(0);
 
-  const [list, setlist] = useState([{ Started: initSate, Time: "0" }]);
-
+  //run
+  const [arr, setarr] = useState([{ Started: new Date(), Time: "0" }]);
+  const [isRunning, setisRunning] = useState(false);
+  const [initRun, setRun] = useState(initStartT);
+  //Data
+  const [Percent, setPercent] = useState(0);
   function handle(event) {
-    setlist((prevItems) => {
-      setBUTT(!BUTT);
-      var A = ReturnToNumber(initSate);
-      var B = ReturnToNumber(prevItems[prevItems.length - 1].Started);
-
-      const Time = Math.floor((A - B) / 60) + ":" + ((A - B) % 60);
-
-      const obj = { Started: initSate, Time };
-      event.preventDefault();
+    setRun(new Date());
+    setarr((prevItems) => {
+      setisRunning(!isRunning);
+      const now = new Date();
+      const Time = Math.floor(now - prevItems[prevItems.length - 1].Started);
+      const obj = { Started: now, Time };
+      //event.preventDefault();
+      event.persist();
       return [...prevItems, obj];
     });
   }
 
+  function startTraining(event) {
+    if (isTraining === 0) {
+      const now = new Date();
+      setStartT(now);
+      setIsTraining(1);
+      setRun(now);
+    }
+    if (isTraining === 1) {
+      setisRunning(false);
+      setIsTraining(2);
+      if (arr.length <= 2) return;
+      let sum = 0;
+      arr.forEach((element, index) => {
+        if (index % 2 === 0 && index !== 0) sum += element.Time;
+      });
+      setPercent(
+        ((sum * 100) / (arr[arr.length - 1].Started - arr[1].Started)).toFixed(
+          2
+        )
+      );
+    }
+    if (isTraining === 2) {
+      setarr([{ Started: new Date(), Time: "0" }]);
+      setIsTraining(0);
+    }
+    setTraining(new Date());
+    //event.preventDefault();
+    event.persist();
+  }
   function getTime() {
-    setSate(
-      new Date().toLocaleTimeString().replace("AM", "").replace("PM", "")
+    setTime(new Date());
+  }
+  function formatTime(A, B) {
+    return (
+      Math.floor((A - B) / 3600000) +
+      ":" +
+      Math.floor((A - B) / 60000) +
+      ":" +
+      (Math.floor((A - B) / 1000) % 60)
     );
   }
-
-  function ReturnToNumber(a) {
-    var arr = a.split(":");
+  function formatTimeNormal(A) {
     return (
-      parseInt(arr[0]) * 60 * 60 + parseInt(arr[1]) * 60 + parseInt(arr[2])
+      Math.floor(A / 3600000) +
+      ":" +
+      Math.floor(A / 60000) +
+      ":" +
+      (Math.floor(A / 1000) % 60)
     );
   }
 
   return (
     <div>
       <div className='formm'>
-        <h1>{initSate}</h1>
-        <button onClick={handle}>
-          {" "}
-          Press to {BUTT ? "Start Running" : "Take a break"}{" "}
-        </button>
+        <div>
+          <h1>{initTime.toLocaleDateString()}</h1>
+          <h2>{initTime.toLocaleTimeString()}</h2>
+          <button onClick={startTraining}>
+            {" "}
+            Press to{" "}
+            {isTraining === 0
+              ? "Start Training"
+              : isTraining === 1
+              ? "End Training"
+              : "Reset"}
+          </button>
+        </div>
+      </div>
+      <div className='formm'>
+        <h1>
+          {isTraining === 1
+            ? formatTime(initTime, initTraining)
+            : isTraining === 0
+            ? "To start Training press 👆"
+            : formatTime(initTraining, initStartT)}
+        </h1>
       </div>
 
-      {list.map((element, index) => {
-        if (index < 1) return null;
+      <div className='formm'>
+        <h2>
+          {isTraining === 1
+            ? formatTime(new Date(), initRun)
+            : isTraining === 0
+            ? "When You are ready press the first yellow buttom"
+            : "Well Done"}
+        </h2>
+        <button
+          style={
+            isRunning
+              ? { backgroundColor: "#f7e7bd" }
+              : { backgroundColor: "#f5ba13" }
+          }
+          onClick={isTraining === 1 ? handle : () => null}
+        >
+          {isTraining === 1
+            ? isRunning
+              ? "Press to Take a Break"
+              : "Press to Start Running"
+            : "You are not Training"}
+        </button>
+      </div>
+      {isTraining === 2 ? (
+        <div className='formm'>
+          <h4>You ran {Percent}% of your training Time</h4>
+        </div>
+      ) : (
+        ""
+      )}
+      {arr.map((element, index) => {
+        if (index < 2) return null;
         return (
           <Card
             key={index}
-            Started={element.Started}
-            Time={element.Time}
+            Started={element.Started.toLocaleTimeString()}
+            Time={formatTimeNormal(element.Time)}
             State={index % 2 ? "Break" : "Run"}
           />
         );
@@ -61,5 +150,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
